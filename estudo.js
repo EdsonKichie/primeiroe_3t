2ea76883bc8e400674a36fdfeeef1df2819edb7f
Edson Kichie <102360300+EdsonKichie@users.noreply.github.com>
2022-11-16 15:16:02 -0300

    Aula 16-11
    Começamos a formatar a Calculadora ESG em JavaScript .

--- estudo.js
+++ estudo.js
@@ -96,3 +96,11 @@ function fecharNotas() {
     r = 180 - r;
     document.getElementById("resultadofechar").innerHTML = "Falta: " + r;
 }
+
+function calculaESG() {
+    let a = document.getElementById("a").value;
+    let b = document.getElementById("b").value;
+    let c = document.getElementById("c").value;
+    let delta = (b*b)-(4*a*c);
+    document.getElementById("").innerHTML
+}
